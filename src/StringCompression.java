bf86e48f7f4980bf31921b2359cef864d2aca821
acisliver <7273322@naver.com>
2021-12-20 20:25:12 +0900

    문자열 압축 문제 수정

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -7,7 +7,6 @@ class StringCompression {
     public int solution(String s) {
         int answer = 0;
 
-        int max_len = 0;
         int counter = 1;
 
         List<String> list = new ArrayList<>();
@@ -15,26 +14,37 @@ class StringCompression {
         int subLen = s.length() / 2;
         for (int i = 2; i < subLen; i++) {
 
-            // i개로 s 자르기
+            // i 크기로 s 자르기
             int j;
             for (j = 0; j < s.length() / i; j++) {
                 list.add(s.substring(i * j, i * (j + 1)));
             }
-            if (i*(j+1) <= s.length())
-                list.add(s.substring(i * (j+1)));
 
-//            // 연속되는 패턴 찾기
-//            for (int p = 0; p < s.length() / i - 1; p++) {
-//                if (list.get(p).equals(list.get(p + 1)))
-//                     counter++;
-//                else
-//                    if (counter > 1) {
-//                        sb.append(counter);
-//                        sb.append(list.get(p - 1));
-//                    }
-//            }
+            // 길이가 i 보다 작은 마지막 남은 s 출력
+            if (i * j < s.length())
+                list.add(s.substring(i * j));
+
+            // 연속되는 패턴 찾기
+            for (int p = 0; p < list.size() - 1; p++) {
+                if (list.get(p).equals(list.get(p + 1)))
+                    counter++;
+                else if (counter > 1) {
+                    sb.append(counter);
+                    sb.append(list.get(p - 1));
+                    counter = 0;
+                }
+                else
+                    sb.append(list.get(p));
+            }
+
+            if (counter > 1) {
+                sb.append(counter);
+
+            }
+
             System.out.println(Arrays.toString(list.toArray()));
             System.out.println(sb);
+            sb = new StringBuilder();
             list = new ArrayList<>();
         }
 
@@ -43,6 +53,6 @@ class StringCompression {
 
     public static void main(String[] args) {
         StringCompression s = new StringCompression();
-        s.solution("abababdede");
+        s.solution("abcabcabcabcdededededede");
     }
 }
