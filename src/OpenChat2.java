52898ca4dea1862ed9dc8f20093543f1a47ee26b
acisliver <7273322@naver.com>
2022-01-06 15:24:52 +0900

    오픈채팅 오타 수정

--- src/OpenChat2.java
+++ src/OpenChat2.java
@@ -8,11 +8,15 @@ public class OpenChat2 {
         openChat.solution(new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"});
     }
 
-    public ArrayList<String> solution(String[] record) {
+    private static final String ENTER_FORMAT = "%s님이 들어왔습니다.";
+    private static final String LEAVE_FORMAT = "%s님이 ";
+
+    public String[] solution(String[] record) {
         ArrayList<String> answer = new ArrayList<>();
         HashMap<String, String> userInfo = new HashMap<>();
         String[] dataList;
 
+        // uid, nickname 매칭
         for (String data : record) {
             dataList = data.split(" ");
             if (dataList[0].equals("Leave"))
@@ -23,24 +27,18 @@ public class OpenChat2 {
 
         System.out.println(userInfo);
 
+        // 출력문 작성
         for (String data : record) {
             dataList = data.split(" ");
             if (dataList[0].equals("Enter"))
-                answer.add(enterMsg(userInfo.get(dataList[1])));
+                answer.add(String.format(ENTER_FORMAT, userInfo.get(dataList[1])));
             else if (dataList[0].equals("Leave"))
-                answer.add(leaveMsg(userInfo.get(dataList[1])));
+                answer.add(String.format(LEAVE_FORMAT, userInfo.get(dataList[1])));
         }
 
         System.out.println(answer);
 
-        return answer;
-    }
-
-    public static String enterMsg(String nickname) {
-        return nickname + "님이 들어왔습니다.";
-    }
-
-    public static String leaveMsg(String nickname) {
-        return nickname + "님이 나갔습니다.";
+        return answer.stream()
+                .toArray(array -> new String[answer.size()]);
     }
 }
