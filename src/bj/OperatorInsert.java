3ccbb6e112ea110107b31fc84a6d2c0a7ef7b4d7
acisliver <7273322@naver.com>
2022-01-14 13:47:43 +0900

    연산자 끼워넣기 - 연산자 개수 배열 연산자 배열로 변환

--- src/bj/OperatorInsert.java
+++ src/bj/OperatorInsert.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// https://www.acmicpc.net/problem/14888
+// 완전탐색에 중첩되는 경우는 백트래킹으로 풀이
 public class OperatorInsert {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,12 +22,43 @@ public class OperatorInsert {
              nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] operator = new int[4];
+        int[] opNum = new int[4];
 
         // 줄바꿈
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+            opNum[i] = Integer.parseInt(st.nextToken());
         }
+
+        String[] operator = numToOperator(n, opNum);
+        System.out.println(Arrays.toString(operator));
+        
+        getMin(nums, opNum);
+        getMax(nums, opNum);
+    }
+
+    private static String[] numToOperator(int n, int[] opNum) {
+        String[] opList = {"+", "-", "*", "/"};
+        String[] operator = new String[n - 1];
+        int idx = 0;
+        int opIdx = 0;
+        for (int op : opNum) {
+            for (int i = 0; i < op; i++) {
+                operator[idx] = opList[opIdx];
+                idx++;
+            }
+            opIdx++;
+        }
+
+        return operator;
+    }
+
+    private static void getMax(int[] nums, int[] operator) {
+
+
+    }
+
+    private static void getMin(int[] nums, int[] operator) {
+
     }
 }
