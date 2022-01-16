6d77bbdaa619bf4120387bce9edc1f73a0bb256c
acisliver <7273322@naver.com>
2022-01-17 07:12:01 +0900

    감시 - cctv5 완

--- src/bj/N15683.java
+++ src/bj/N15683.java
@@ -33,21 +33,49 @@ public class N15683 {
     private static void recursive(int N, int M, String[][] office, int idx) {
         int i, j;
         if (N * M == idx) {
-
+            System.out.println(Arrays.deepToString(office));
         } else {
             i = idx / N;
             j = idx % M;
             switch (office[i][j]) {
                 case "5":
-                    for (int k = 0; k < N; k++)
+                    // 열 "#"으로 변환
+                    for (int k = 0; k < N; k++) {
+                        // 벽인 경우
+                        if (office[k][j].equals("6")) {
+                            // 벽이 5보다 아래 있을 경우 변환 종료
+                            if (k > i) break;
+                            // 벽이 5보다 위에 있을 경우 이전 변환 0으로 복구
+                            else {
+                                for (int l = 0; l < k; l++) {
+                                    office[k][j] = "0";
+                                }
+                            }
+                        }
+                        // 빈 공간인 경우
                         if (office[k][j].equals("0")) office[k][j] = "#";
+                    }
 
-                    for (int l = 0; l < M; l++)
+
+                    for (int l = 0; l < M; l++) {
+                        // 벽인 경우
+                        if (office[i][l].equals("6")) {
+                            // 벽이 5보다 오른쪽에 있을 경우
+                            if (l > j) break;
+                            else {
+                                for (int k = 0; k < office.length; k++) {
+                                    office[i][k] = "0";
+                                }
+                            }
+                        }
+                        // 빈 공간일 경우
                         if (office[i][l].equals("0")) office[i][l] = "#";
+                    }
 
+                    recursive(N, M, office, idx + 1);
+                    break;
             }
-            recursive(N, M, office, idx + 1);
+            System.out.println(Arrays.deepToString(office));
         }
-        System.out.println(Arrays.deepToString(office));
     }
 }
