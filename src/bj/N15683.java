da3338b9b4d7c0f615b82a336fee375c5ad8daaf
acisliver <7273322@naver.com>
2022-01-17 07:21:33 +0900

    감시 - cctv5 완2

--- src/bj/N15683.java
+++ src/bj/N15683.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/15683
 // 감시
 public class N15683 {
+    private static int MIN;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,6 +20,7 @@ public class N15683 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         String[][] office = new String[N][M];
+        MIN = N * M;
 
         for (int i = 0; i < N; i++) {
             office[i] = br.readLine().split(" ");
@@ -28,11 +31,14 @@ public class N15683 {
 
     private static void solution(int N, int M, String[][] office) {
         recursive(N, M, office, 0);
+        System.out.println(MIN);
     }
 
     private static void recursive(int N, int M, String[][] office, int idx) {
         int i, j;
         if (N * M == idx) {
+            MIN = countBlindSpot(office);
+            System.out.println(MIN);
             System.out.println(Arrays.deepToString(office));
         } else {
             i = idx / N;
@@ -48,14 +54,16 @@ public class N15683 {
                             // 벽이 5보다 위에 있을 경우 이전 변환 0으로 복구
                             else {
                                 for (int l = 0; l < k; l++) {
+                                    // cctv인 경우
+                                    if (Integer.parseInt(office[k][j]) > 0) break;
                                     office[k][j] = "0";
                                 }
+
                             }
                         }
                         // 빈 공간인 경우
                         if (office[k][j].equals("0")) office[k][j] = "#";
                     }
-
 
                     for (int l = 0; l < M; l++) {
                         // 벽인 경우
@@ -64,6 +72,8 @@ public class N15683 {
                             if (l > j) break;
                             else {
                                 for (int k = 0; k < office.length; k++) {
+                                    // cctv인 경우
+                                    if (Integer.parseInt(office[i][k]) > 0) break;
                                     office[i][k] = "0";
                                 }
                             }
@@ -74,8 +84,20 @@ public class N15683 {
 
                     recursive(N, M, office, idx + 1);
                     break;
+                default:
+                    recursive(N, M, office, idx + 1);
             }
-            System.out.println(Arrays.deepToString(office));
+
         }
+    }
+
+    private static int countBlindSpot(String[][] office) {
+        int count = 0;
+        for (int i = 0; i < office.length; i++) {
+            for (int j = 0; j < office[0].length; j++)
+                if (office[i][j].equals("0")) count++;
+        }
+
+        return count;
     }
 }
