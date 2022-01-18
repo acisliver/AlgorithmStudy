82cb2b3ba42867c5de16bcec5d145912f8bc01d5
acisliver <7273322@naver.com>
2022-01-18 22:23:58 +0900

    A * B

---
+++ src/bj/N10998.java
@@ -0,0 +1,12 @@
+package bj;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/10998
+// A * B
+public class N10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * sc.nextInt());
+    }
+}
