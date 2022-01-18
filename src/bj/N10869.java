0c95d66041e29eb6543f6ae3cd24df63c5a9c88d
acisliver <7273322@naver.com>
2022-01-18 22:25:55 +0900

    사칙연산

---
+++ src/bj/N10869.java
@@ -0,0 +1,18 @@
+package bj;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/10869
+// 사칙연산
+public class N10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
