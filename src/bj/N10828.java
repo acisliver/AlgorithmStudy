5c17b75bd06e87d66072047f0616f026cfcd5d9a
acisliver <7273322@naver.com>
2022-01-24 14:17:04 +0900

    스택 - 배열로 구현 (시간초과)

--- src/bj/N10828.java
+++ src/bj/N10828.java
@@ -1,14 +1,13 @@
 package bj;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 // https://www.acmicpc.net/problem/10828
 // 스택
 public class N10828 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Stack stack = new Stack();
+        ArrayStack stack = new ArrayStack();
         int commands = sc.nextInt();
         String command;
         for (int i = 0; i < commands; i++) {
@@ -18,45 +17,60 @@ public class N10828 {
                     stack.push(sc.nextInt());
                     break;
                 case "pop":
-                    stack.pop();
+                    System.out.println(stack.pop());
                     break;
-
-
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.empty());
+                    break;
+                case "top":
+                    System.out.println(stack.top());
+                    break;
             }
 
         }
 
     }
 
-    public static class Node {
-        public Node node;
-        private int value;
+    public static class ArrayStack {
+        int TOTAL_SIZE = 10000;
+        int[] data;
+        int top;
 
-        public Node() {
-        }
-
-        public Node(int value) {
-            this.value = value;
-        }
-    }
-
-    public static class Stack {
-        public Node head;
-        public int top;
-
-        public Stack() {
-            this.head = new Node();
+        ArrayStack() {
+            data = new int[TOTAL_SIZE];
             this.top = -1;
         }
 
-        public void push(int value) {
-            Node pushNode = new Node(value);
-            this.top++;
+        public int empty() {
+            return this.top == -1 ? 1 : 0;
         }
 
-        public void pop() {
-            this.top--;
+        public boolean full() {
+            return this.top == this.TOTAL_SIZE - 1;
+        }
+
+        public void push(int data) {
+            if (full()) {
+                System.out.println("스택 꽉 참");
+            }
+            this.data[++this.top] = data;
+        }
+
+        public int pop() {
+            return empty() == 1 ? -1 : this.data[this.top--];
+        }
+
+        public int size() {
+            return this.top + 1;
+        }
+
+        public int top() {
+            return empty() == 1 ? -1 : this.data[top];
         }
     }
+
 
 }
