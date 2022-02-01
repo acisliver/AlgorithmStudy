81ac91bb0efe3d8f0293138907f8160f6e7fbc64
acisliver <7273322@naver.com>
2022-02-02 05:10:33 +0900

    최소힙

--- src/bj/N1927.java
+++ src/bj/N1927.java
@@ -1,39 +1,73 @@
 package bj;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/1927
 // 최소 힙
 public class N1927 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] heap = new int[100001];
+        int size = 0;
         for (int i = 0; i < n; i++) {
+            int v = sc.nextInt();
+            if (v == 0) {
+                if (size == 0) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(removeMin(heap, size));
+                    size--;
+                }
+            } else {
+                size++;
+                add(heap, size, v);
+            }
+        }
 
+    }
+
+    public static void add(int[] heap, int size, int value) {
+        int tmp;
+        int s = size;
+        heap[size] = value;
+        while(true) {
+            if (heap[s/2] > value) {
+                tmp = heap[s/2];
+                heap[s/2] = value;
+                heap[s] = tmp;
+                s /= 2;
+            } else {
+                break;
+            }
         }
     }
-    public class Node {
-        public int value;
-        public Node left;
-        public Node right;
 
-        public Node() {
-
+    public static int removeMin(int[] heap, int size) {
+        int re = heap[1];
+        heap[1] = heap[size];
+        heap[size] = 0;
+        int parent = 1;
+        int child;
+        int tmp;
+        while(true) {
+            child = parent * 2;
+            // 자식 중 최솟값 찾기
+            if (child + 1 <= size - 1 && heap[child] > heap[child + 1]) {
+                child++;
+            }
+            // child 값이 힙을 넘어가거나 자식의 값이 부모의 값보다 클 때
+            if (child > size - 1 || heap[child] > heap[parent]) {
+                break;
+            }
+            // 자식과 부모의 값을 바꾸어줌
+            tmp = heap[parent];
+            heap[parent] = heap[child];
+            heap[child] = tmp;
+            parent = child;
         }
-    }
 
-    public class MinHeap {
-        public Node root;
-
-        public MinHeap() {
-            this.root = new Node();
-        }
-
-        public boolean add(int value) {
-
-            return false;
-        }
+        return re;
     }
 }
