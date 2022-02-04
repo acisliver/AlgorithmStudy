6606d5aa0724076d4e8ee6dda279a0b8b4742bdf
acisliver <7273322@naver.com>
2022-02-04 09:03:07 +0900

    외판원 순회 - 백트래킹 (수정 및 주석 추가)

--- src/bj/N2098.java
+++ src/bj/N2098.java
@@ -23,31 +23,36 @@ public class N2098 {
                     .toArray();
         }
 
+        // 0번 정점부터 시작
         tsp(0, 0, 0);
         System.out.println(MIN);
     }
 
     private static void tsp(int at, int weight, int visited) {
-        if (valid(weight)) {
-            if (visited == (1 << N) - 1) {
+        // 탐색을 완료했다면
+        if (visited == (1 << N) - 1) {
+            if (MIN > weight) {
                 MIN = weight;
                 return;
             }
-            for (int next = 1; next < N; next++) {
-                if ((visited & (1 << next)) != (1 << next) && WEIGHTS[at][next] != 0) {
+        }
+        for (int next = 1; next < N; next++) {
+            // 아직 방문하지 않았고 간선이 있다면
+            if ((visited & (1 << next)) != (1 << next) && WEIGHTS[at][next] != 0) {
+                // 가지치기 조건
+                if (weight + WEIGHTS[at][next] < MIN) {
                     visited |= 1 << next;
                     tsp(next, weight + WEIGHTS[at][next], visited);
-                    visited &= ~(1<< next);
+                    visited &= ~(1 << next);
                 }
             }
-            if ((visited == (1 << N) - 2) && WEIGHTS[at][0] != 0) {
+        }
+        if ((visited == (1 << N) - 2) && WEIGHTS[at][0] != 0) {
+            if (weight + WEIGHTS[at][0] < MIN) {
                 visited |= 1;
                 tsp(0, weight + WEIGHTS[at][0], visited);
             }
         }
-    }
 
-    private static boolean valid(int weight) {
-        return weight <= MIN;
     }
 }
