f0a651164b0525b17def64cae3acde04b52e9cda
acisliver <7273322@naver.com>
2022-02-06 17:57:57 +0900

    회의실 배정 - 빨리 끝나는 작업 그리디

---
+++ src/bj/N1931.java
@@ -0,0 +1,37 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// https://www.acmicpc.net/problem/1931
+// 회의실 배정
+public class N1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<int[]> jobList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            jobList.add(new int[] {start, end});
+        }
+
+        jobList.sort(Comparator.comparingInt(j -> j[0]));
+        jobList.sort(Comparator.comparingInt(j -> j[1]));
+
+        int endTime = 0;
+        int count = 0;
+        for (int[] job : jobList) {
+            if (endTime <= job[0]) {
+                endTime = job[1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
