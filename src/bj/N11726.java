e5dbb114488406d48821f23b242e306119dc7c1f
acisliver <7273322@naver.com>
2022-02-07 22:30:20 +0900

    2xn 타일링 - 수정

--- src/bj/N11726.java
+++ src/bj/N11726.java
@@ -13,7 +13,6 @@ public class N11726 {
 
         dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 3;
         for (int i = 2; i < n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
