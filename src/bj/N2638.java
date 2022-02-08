ee16764f71816b805ee1fde542a04121191d7df0
acisliver <7273322@naver.com>
2022-02-08 11:48:26 +0900

    치즈

--- src/bj/N2638.java
+++ src/bj/N2638.java
@@ -3,119 +3,87 @@ package bj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class N2638 {
-    private static int time = 0;
+    private static int n, m;
+    private static final int[] DX = {0, 0, -1, 1};
+    private static final int[] DY = {-1, 1, 0, 0};
+    private static int cheeseCount = 0;
+    private static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int time = 0;
 
         int[][] cheese = new int[n][m];
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 cheese[i][j] = Integer.parseInt(st.nextToken());
+                if (cheese[i][j] == 1) cheeseCount++;
             }
         }
 
-        // 내부(0), 외부(2) 공기 구분
-        deleteExternalAir(cheese, 0, 0);
-
-        while (isCheese(cheese)) {
-
-            ArrayList<int[]> outCheeseIndex = getMeltingCheese(cheese);
+        while(cheeseCount != 0) {
             time++;
-            meltCheese(cheese, outCheeseIndex);
-            for (int i = 0; i < cheese.length; i++) {
-                for (int j = 0; j < cheese.length; j++) {
-                    if (cheese[i][j] == 0) {
-                        for (int k = -1; k < 2; k++) {
-                            for (int l = -1; l < 2; l++) {
-                                try {
-                                    if (k * l == 0 && cheese[i + k][j + l] == 2)
-                                        deleteExternalAir(cheese, i + k, j + l);
-                                } catch (ArrayIndexOutOfBoundsException ignored) {
-                                }
-                            }
-
-                        }
-
-                    }
-
-                }
-            }
+            isOut(cheese);
+            melt(cheese);
         }
 
         System.out.println(time);
     }
 
-    // 외부 공기는 2로 표현
-    private static void deleteExternalAir(int[][] cheese, int i, int j) {
-        cheese[i][j] = 2;
+    private static void isOut(int[][] cheese) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited = new boolean[n][m];
+        queue.offer(new int[]{0, 0});
+        cheese[0][0] = 2;
+        while (!queue.isEmpty()) {
+            int[] index = queue.poll();
+            int curY = index[0];
+            int curX = index[1];
+            for (int k = 0; k < 4; k++) {
+                if (curY + DY[k] < 0 || curY + DY[k] >= n || curX + DX[k] < 0 || curX + DX[k] >= m)
+                    continue;
 
-        for (int k = -1; k < 2; k++) {
-            for (int l = -1; l < 2; l++) {
-                try {
-                    if (k * l == 0 && cheese[i + k][j + l] == 0)
-                        deleteExternalAir(cheese, i + k, j + l);
-                } catch (ArrayIndexOutOfBoundsException ignored) {
+                if (visited[curY + DY[k]][curX + DX[k]] || cheese[curY + DY[k]][curX + DX[k]] == 1)
+                    continue;
+
+
+                cheese[curY + DY[k]][curX + DX[k]] = 2;
+                queue.offer(new int[]{curY + DY[k], curX + DX[k]});
+                visited[curY + DY[k]][curX + DX[k]] = true;
+
+            }
+        }
+    }
+
+    private static void melt(int[][] cheese) {
+        int meltCount = 0;
+        for (int curY = 0; curY < cheese.length; curY++) {
+            for (int curX = 0; curX < cheese[0].length; curX++) {
+                if (cheese[curY][curX] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        if (curY + DY[k] < 0 || curY + DY[k] >= n || curX + DX[k] < 0 || curX + DX[k] >= m) {
+                            continue;
+                        }
+
+                        if (cheese[curY + DY[k]][curX + DX[k]] == 2) {
+                            meltCount++;
+                        }
+                    }
+                    if (meltCount > 1) {
+                        cheeseCount--;
+                        cheese[curY][curX] = 3;
+                    }
+                    meltCount = 0;
                 }
             }
-
         }
-    }
-
-    // 치즈 녹이기(1 -> 0) 한 번에 녹여야함
-    private static void meltCheese(int[][] cheese, ArrayList<int[]> outCheeseIndex) {
-        for (int[] cheeseIndex : outCheeseIndex) {
-            cheese[cheeseIndex[0]][cheeseIndex[1]] = 2;
-        }
-    }
-
-    // 1시간 뒤 녹을 치즈의 위치 탐색
-    private static ArrayList<int[]> getMeltingCheese(int[][] cheese) {
-        ArrayList<int[]> outCheeseIndex = new ArrayList<>();
-
-        for (int i = 1; i < (cheese.length - 1); i++) {
-            for (int j = 1; j < (cheese[0].length - 1); j++) {
-                if (cheese[i][j] == 1)
-                    if (contactAir(cheese, i, j)) {
-                        int[] idx = {i, j};
-                        outCheeseIndex.add(idx);
-                    }
-            }
-        }
-        return outCheeseIndex;
-    }
-
-    // 외부 공기와 접촉(주변에 2칸 이상 0)인지 확인
-    private static boolean contactAir(int[][] cheese, int i, int j) {
-        int surface = 0;
-
-        for (int k = -1; k < 2; k++) {
-            for (int l = -1; l < 2; l++) {
-                // 외부 공기와 접촉하는지
-                if (cheese[i + k][j + l] == 2 && l * k == 0) surface++;
-            }
-        }
-        return surface > 1;
-    }
-
-    // 치즈가 남아있는지 확인
-    private static boolean isCheese(int[][] cheese) {
-        for (int[] row : cheese) {
-            for (int c : row) {
-                if (c == 1) return true;
-            }
-        }
-        return false;
     }
 }
