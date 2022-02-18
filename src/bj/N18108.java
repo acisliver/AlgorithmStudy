47ee1f55c6e9eb5ce3b85efc80641a9c046052ef
acisliver <7273322@naver.com>
2022-02-18 23:09:17 +0900

    1992년생인 내가 태국에서는 2541년생?!

---
+++ src/bj/N18108.java
@@ -0,0 +1,12 @@
+package bj;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/18108
+// 1992년생인 내가 태국에서는 2541년생?!
+public class N18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - 543);
+    }
+}
