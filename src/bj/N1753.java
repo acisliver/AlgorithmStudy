453b4827aaa9536ac909382c9333ec513ae778c3
acisliver <7273322@naver.com>
2022-03-07 11:25:17 +0900

    최단거리 - 시간 초과(우선순위큐로 바꿔볼 것)

--- src/bj/N1753.java
+++ src/bj/N1753.java
@@ -3,14 +3,22 @@ package bj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.IntStream;
 
 // https://www.acmicpc.net/problem/1753
 // 최단경로
 public class N1753 {
     static int V, E, START;
+
+    static class Edge {
+        int node, weight;
+
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,13 +26,15 @@ public class N1753 {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         START = Integer.parseInt(br.readLine()) - 1;
-        int[][] graph = new int[V][V];  // 인접행렬
-        for (int[] ints : graph) {
-            Arrays.fill(ints, 11);
-        }
+
+        ArrayList<LinkedList<Edge>> graph = new ArrayList<>(V); // 인접리스트
+        Collections.fill(graph, new LinkedList<>());
+        IntStream.range(0, V + 1)
+                .forEach(i -> graph.add(new LinkedList<>()));
+
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            graph[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = Integer.parseInt(st.nextToken());
+            graph.get(Integer.parseInt(st.nextToken()) - 1).add(new Edge(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken())));
         }
 
         // 1. distance를 무한대로 초기화
@@ -37,10 +47,8 @@ public class N1753 {
         visited |= 1 << (START);
 
         // 3. 시작노드와 인접한 노드들의 distance값 갱신
-        for (int i = 0; i < distance.length; i++) {
-            if (graph[START][i] != 11) {
-                distance[i] = graph[START][i];
-            }
+        for (Edge edge : graph.get(START)) {
+            distance[edge.node] = edge.weight;
         }
 
         while (Integer.bitCount(visited) < V) { // 모든 노드를 방문할 때까지 반복
@@ -49,13 +57,12 @@ public class N1753 {
             // 5. 최소인 정점을 방문
             visited |= 1 << (minVertex);
             // 최소인 정점과 연결된 정점들의 distance 갱신
-            for (int i = 0; i < distance.length; i++) {
-                if ((visited & (1 << i)) == 0) {
-                    if (distance[minVertex] + graph[minVertex][i] < distance[i]) {
-                        distance[i] = distance[minVertex] + graph[minVertex][i];
+            for (Edge edge : graph.get(minVertex)) {
+                if ((visited & (1 << edge.node)) == 0) {
+                    if (distance[minVertex] + edge.weight < distance[edge.node]) {
+                        distance[edge.node] = distance[minVertex] + edge.weight;
                     }
                 }
-
             }
         }
 
