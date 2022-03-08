f602c83f5f3dc41a303035b510f653017552da9a
acisliver <7273322@naver.com>
2022-03-08 14:04:56 +0900

    최단거리 - 성공 및 리팩토링 (실수가 있었음)

--- src/bj/N1753.java
+++ src/bj/N1753.java
@@ -12,25 +12,16 @@ public class N1753 {
     static int V, E, START;
 
     static class Node implements Comparable<Node> {
-        int node, dis;
+        int node, weight;
 
-        public Node(int node, int dis) {
+        public Node(int node, int weight) {
             this.node = node;
-            this.dis = dis;
+            this.weight = weight;
         }
 
         @Override
         public int compareTo(Node node) {
-            return this.dis - node.dis;
-        }
-    }
-
-    static class Edge {
-        int node, weight;
-
-        public Edge(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
+            return this.weight - node.weight;
         }
     }
 
@@ -41,64 +32,64 @@ public class N1753 {
         E = Integer.parseInt(st.nextToken());
         START = Integer.parseInt(br.readLine()) - 1;
 
-        ArrayList<LinkedList<Edge>> graph = new ArrayList<>(V); // 인접리스트
-        IntStream.range(0, V + 1)
-                .forEach(i -> graph.add(new LinkedList<>()));
+        ArrayList<LinkedList<Node>> graph = new ArrayList<>(V); // 인접리스트
+        for (int i = 0; i < V + 1; i++) {
+            graph.add(new LinkedList<>());
+        }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            graph.get(Integer.parseInt(st.nextToken()) - 1).add(new Edge(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken())));
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int weight = Integer.parseInt(st.nextToken());
+            graph.get(start).add(new Node(end, weight));
         }
 
         // 1. distance를 무한대로 초기화
         PriorityQueue<Node> pq = new PriorityQueue<>();
         int[] distance = new int[V];
         Arrays.fill(distance, Integer.MAX_VALUE);
-        int[] visited = new int[V];
-        Arrays.fill(visited, 0);
+        boolean[] visited = new boolean[V];
 
         // 2. 시작노드의 거리를 0으로
         pq.offer(new Node(START, 0));
         distance[START] = 0;
-        visited[START] = 1;
 
         // 3. 시작노드와 인접한 노드들의 distance값 갱신
         graph.get(START)
                 .forEach(edge -> pq.offer(new Node(edge.node, edge.weight)));
-        for (Edge edge : graph.get(START)) {
-            distance[edge.node] = edge.weight;
+        for (Node node : graph.get(START)) {
+            distance[node.node] = node.weight;
         }
 
         while (!pq.isEmpty()) { // 모든 노드를 방문할 때까지 반복
-            if (Arrays.stream(visited)
-                    .filter(v -> v == 1)
-                    .count() == V)
-                break;
-
             // 4. 방문하지 않은 노드 중 distance 값이 최소인 정점을 찾음
             // 5. 최소인 정점을 방문
             Node curNode = pq.poll();
-            if (visited[curNode.node] == 1) continue;
-            visited[curNode.node] = 1;
 
-
+            // 방문했는지 체크
+            if (visited[curNode.node]) continue;
+            visited[curNode.node] = true;
 
             // 최소인 정점과 연결된 정점들의 distance 갱신
-            for (Edge edge : graph.get(curNode.node)) {
-                if (curNode.dis + edge.weight < distance[edge.node]) {
-                    distance[edge.node] = curNode.dis + edge.weight;
-                    pq.offer(new Node(edge.node, distance[edge.node]));
+            for (Node node : graph.get(curNode.node)) {
+                if (distance[curNode.node] + node.weight < distance[node.node]) {
+                    distance[node.node] = distance[curNode.node] + node.weight;
+                    pq.offer(new Node(node.node, distance[node.node]));
                 }
             }
         }
+
         // 출력
         StringBuilder sb = new StringBuilder();
         Arrays.stream(distance)
-                .mapToObj(d -> {
-                    if (d == Integer.MAX_VALUE) return "INF";
-                    else return String.valueOf(d);
-                })
-                .forEach(v -> sb.append(v).append("\n"));
+                .forEach(v -> {
+                    if (v == Integer.MAX_VALUE) {
+                        sb.append("INF").append("\n");
+                    } else {
+                        sb.append(v).append("\n");
+                    }
+                });
         System.out.println(sb);
     }
 }
