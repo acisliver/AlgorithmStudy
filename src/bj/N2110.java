5c4d83997d38cf59cc1fc581314c160fbff0f8bc
acisliver <7273322@naver.com>
2022-04-20 21:59:54 +0900

    공유기 설치 - 이진 탐색

--- src/bj/N2110.java
+++ src/bj/N2110.java
@@ -3,12 +3,16 @@ package bj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/2110
 // 공유기 설치
 // 이진 탐색
 public class N2110 {
+    static int[] home;
+    static long min, max;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,18 +20,44 @@ public class N2110 {
         int n = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        int[] home = new int[n];
-        boolean[] router = new boolean[n];
+        home = new int[n];
 
         for (int i = 0; i < n; i++) {
             home[i] = Integer.parseInt(br.readLine());
         }
 
-        // 양 끝집은 라우터 배치
-        router[0] = true;
-        router[n - 1] = true;
-        c -= 2;
+        Arrays.sort(home);
 
+        min = 1;
+        max = home[n - 1] - home[0] + 1;
 
+        // mid값보다 작은 거리로 공유기 배치
+        while(min < max) {
+            long mid = (min + max) >>> 1;
+
+            if (canLocate(mid) < c) {   // 공유기가 남는다면
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(min - 1);
+    }
+
+    public static int canLocate(long distance) {
+        int count = 1;
+        long lastLocated = home[0];
+
+        for (int i = 1; i < home.length; i++) {
+            int curLocate = home[i];
+
+            if (curLocate - lastLocated > distance) {
+                count++;
+                lastLocated = curLocate;
+            }
+        }
+
+        return count;
     }
 }
