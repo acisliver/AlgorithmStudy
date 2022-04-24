95e91b337bb8a8eae932d814b179be2f40543c2a
acisliver <7273322@naver.com>
2022-04-24 22:44:16 +0900

    양궁대회 - 주석 추가

--- src/programmers/kakao/Blind2022_4.java
+++ src/programmers/kakao/Blind2022_4.java
@@ -11,7 +11,7 @@ public class Blind2022_4 {
 
     public static void main(String[] args) {
         Blind2022_4 s = new Blind2022_4();
-        System.out.println(Arrays.toString(s.solution(1, new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})));
+        System.out.println(Arrays.toString(s.solution(9, new int[]{0,0,1,2,0,1,1,1,1,1,1})));
     }
     public int[] solution(int n, int[] apeachInfo) {
 
@@ -39,16 +39,18 @@ public class Blind2022_4 {
         }
 
         // (10 - index)점을 얻을지 말지 결정해 dfs
+        // 라이언이 가장 큰 점수 차이로 우승할 수 있는 방법이 여러가지일 경우, 가장 낮은 점수를 더 많이 맞힌 경우를 return
+        // 점수를 얻지 못할 경우 0점으로 화살을 사용하자
         for (int i = index; i < 11; i++) {
             int lionHit;    // 라이언이 (10 - index)점을 얻기위해 맞춰야하는 화살의 수
-            if (i == 10) {
+            if (i == 10) {  // 마지막 0점을 획득하는 경우 남은 화살을 모두 0점에 쏜다
                 lionHit = n - count;
-            } else {
+            } else {        // 어피치보다 많이 쏴야하기 때문
                 lionHit = apeachInfo[i] + 1;
             }
-            if (lionHit + count > n) continue;
+            if (lionHit + count > n) continue;  // 화살이 더 필요할 경우 해당 경우는 불가능
             lionInfo[i] = lionHit;
-            dfs(n, apeachInfo, count + lionHit, score + 10 - i, lionInfo, i + 1);
+            dfs(n, apeachInfo, count + lionHit, score + 10 - i, lionInfo.clone(), i + 1);
             lionInfo[i] = 0;
         }
     }
@@ -58,7 +60,7 @@ public class Blind2022_4 {
         int lionScore = 0;
         int apeachScore = 0;
 
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i <= 10; i++) {
             if (apeachInfo[i] < lionInfo[i]) {
                 lionScore += 10 - i;
             } else {
