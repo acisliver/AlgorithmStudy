3bb81ac1c64dcc1abceb9f2f493a5d2bfe39edbf
acisliver <7273322@naver.com>
2022-05-06 22:53:16 +0900

    보석 쇼핑 - 완

--- src/programmers/kakao/Intern2020_3.java
+++ src/programmers/kakao/Intern2020_3.java
@@ -21,32 +21,33 @@ public class Intern2020_3 {
     public int[] solution(String[] gems) {
 
         int left = 0;
-        int right = 0;
+        int right = -1;
         int start = 0;
         int end = 0;
         int min = 100_000;
         Set<String> set = new HashSet<>(Arrays.asList(gems));
+        HashMap<String, Integer> map = new HashMap<>();
 
-        while(left < gems.length && right < gems.length) {
-            HashMap<String, Integer> map = new HashMap<>();
+        while(left < gems.length) {
 
-            for (int i = left; i <= right; i++) {
-                map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
-            }
-
-            if (map.size() == set.size()) {
+            if (map.size() == set.size()) { // 모든 보석이 있을 경우
                 if (min > right - left) {
                     min = right - left;
                     start = left + 1;
                     end = right + 1;
                 }
+                map.put(gems[left], map.getOrDefault(gems[left], 0) - 1);
+                if (map.get(gems[left]) == 0) {
+                    map.remove(gems[left]);
+                }
                 left++;
-            } else {
+            } else {                        // 보석이 더 필요한 경우
                 right++;
+                if (right >= gems.length) break;
+                map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
             }
         }
 
         return new int[]{start, end};
     }
-
 }
